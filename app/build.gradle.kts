8ca571820b91a9083335d841bdb43ca4afa50feb
David Gaspar <davidgaspar.dev@gmail.com>
2025-01-11 11:09:23 -0300

    deps: Update targetSdk from 34 to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "dev.davidgaspar.getthis"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "dev.davidgaspar.getthis"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
