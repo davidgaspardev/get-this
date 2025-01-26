e9e5e8894e8986819a26144b18ca8d3dc1dec043
David Gaspar <davidgaspar.dev@gmail.com>
2025-01-25 23:12:59 -0300

    feat: Add Details screen

---
+++ app/src/main/java/dev/davidgaspar/getthis/ui/detail/DetailFragment.kt
@@ -0,0 +1,6 @@
+package dev.davidgaspar.getthis.ui.detail
+
+import androidx.fragment.app.Fragment
+
+class DetailFragment: Fragment() {
+}
---
+++ app/src/main/java/dev/davidgaspar/getthis/ui/detail/DetailViewModel.kt
@@ -0,0 +1,7 @@
+package dev.davidgaspar.getthis.ui.detail
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class DetailViewModel(application: Application): AndroidViewModel(application) {
+}
