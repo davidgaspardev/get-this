c3e7c7e76bc494a2d4f4c91f7af7813c4dbfc259
David Gaspar <davidgaspar.dev@gmail.com>
2025-02-19 23:35:47 -0300

    feat: Check if radio button isn't clicked to show toast message

--- app/src/main/java/dev/davidgaspar/getthis/ui/home/HomeFragment.kt
+++ app/src/main/java/dev/davidgaspar/getthis/ui/home/HomeFragment.kt
@@ -1,9 +1,11 @@
 package dev.davidgaspar.getthis.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import dev.davidgaspar.getthis.databinding.HomeFragmentBinding
 import dev.davidgaspar.getthis.ui.components.LoadingButton
@@ -17,8 +19,15 @@ class HomeFragment: Fragment() {
     ): View {
         val binding = HomeFragmentBinding.inflate(inflater, container, false)
         val view = binding.root
+        val viewModel = HomeViewModel(requireActivity().application)
 
-        binding.loadingButtonContainer.addView(LoadingButton(requireContext()))
+        binding.viewModel = viewModel
+
+        viewModel.toastMessage.observe(viewLifecycleOwner) { message ->
+            message?.let {
+                Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+            }
+        }
 
         return view
     }
--- app/src/main/java/dev/davidgaspar/getthis/ui/home/HomeViewModel.kt
+++ app/src/main/java/dev/davidgaspar/getthis/ui/home/HomeViewModel.kt
@@ -1,8 +1,39 @@
 package dev.davidgaspar.getthis.ui.home
 
 import android.app.Application
+import android.util.Log
+import android.widget.RadioGroup
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 
-class HomeViewModel(application: Application): AndroidViewModel(application) {
+class HomeViewModel(
+    application: Application,
+): AndroidViewModel(application) {
 
+    private val _selectedRadioButtonId = MutableLiveData<Int?>()
+    val selectedRadioButtonId: LiveData<Int?> get() = _selectedRadioButtonId
+
+    fun setSelectedRadioButtonId(id: Int) {
+        _selectedRadioButtonId.value = id
+    }
+
+    private val _toastMessage = MutableLiveData<String?>()
+    val toastMessage: LiveData<String?> get() = _toastMessage
+
+    private fun setToastMessage(message: String) {
+        _toastMessage.value = message
+    }
+
+    fun onClickLoadingButton() {
+        selectedRadioButtonId.value.let { radioButtonId ->
+            if (radioButtonId == null) {
+                setToastMessage("Please select a radio button")
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "HomeViewModel"
+    }
 }
