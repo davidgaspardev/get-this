a1f1799be24c9a4aa94e3e034a36d89eb58b442a
David Gaspar <davidgaspar.dev@gmail.com>
2025-04-23 22:45:31 -0300

    feat: Add notification channel setup for download details

--- app/src/main/java/dev/davidgaspar/getthis/MainActivity.kt
+++ app/src/main/java/dev/davidgaspar/getthis/MainActivity.kt
@@ -1,15 +1,21 @@
 package dev.davidgaspar.getthis
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.messaging.FirebaseMessaging
+import dev.davidgaspar.getthis.services.utils.DOWNLOAD_DETAILS_CHANNEL_ID
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         subscribeMessageToTopic()
+        setupNotification()
     }
 
     private fun subscribeMessageToTopic() {
@@ -21,6 +27,25 @@ class MainActivity : AppCompatActivity() {
                 }
                 Log.d(TAG, msg)
             }
+    }
+
+    private fun setupNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                DOWNLOAD_DETAILS_CHANNEL_ID,                    // ID
+                "Download Details",                  // Name shown to user
+                NotificationManager.IMPORTANCE_HIGH // Importance
+            ).apply {
+                description = "Notifications for viewing details of downloaded files"
+            }
+
+            val notificationManager: NotificationManager = getSystemService(
+                Context.NOTIFICATION_SERVICE
+            ) as NotificationManager
+
+            // Create notification channel
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     companion object {
