17eb1688e6eee84b68ec82084a5f45205b575f68
David Gaspar <davidgaspar.dev@gmail.com>
2025-05-11 23:20:17 -0300

    feat: Enhance DetailFragment to display download information using ViewModel and data binding

--- app/src/main/java/dev/davidgaspar/getthis/ui/detail/DetailFragment.kt
+++ app/src/main/java/dev/davidgaspar/getthis/ui/detail/DetailFragment.kt
@@ -1,6 +1,33 @@
 package dev.davidgaspar.getthis.ui.detail
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.gson.Gson
+import dev.davidgaspar.getthis.data.model.DownloadInfo
+import dev.davidgaspar.getthis.databinding.DetailFragmentBinding
 
 class DetailFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = DetailFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        val application = requireActivity().application
+        val downloadInfoJson = arguments?.getString("downloadInfoJson")
+        val downloadInfo = Gson().fromJson(downloadInfoJson, DownloadInfo::class.java)
+        val viewModel = DetailViewModel(
+            application,
+            downloadInfo
+        )
+
+        binding.viewModel = viewModel
+
+        return view
+    }
 }
--- app/src/main/java/dev/davidgaspar/getthis/ui/detail/DetailViewModel.kt
+++ app/src/main/java/dev/davidgaspar/getthis/ui/detail/DetailViewModel.kt
@@ -2,6 +2,15 @@ package dev.davidgaspar.getthis.ui.detail
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import dev.davidgaspar.getthis.data.model.DownloadInfo
 
-class DetailViewModel(application: Application): AndroidViewModel(application) {
+class DetailViewModel(
+    application: Application,
+    downloadInfo: DownloadInfo,
+): AndroidViewModel(application) {
+
+    private val _downloadInfo = MutableLiveData<DownloadInfo>(downloadInfo)
+    val downloadInfo: LiveData<DownloadInfo> get() = _downloadInfo
 }
